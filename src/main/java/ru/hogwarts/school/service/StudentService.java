a50e9884f5a115f170b99b66ddab268f6dbbc8b3
Bodnevich <bodnevichkristina@mail.ru>
2026-02-08 00:56:35 +0300

    Hogwards SchoolHW5

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -108,6 +108,6 @@ public class StudentService {
         if (student != null && student.getFaculty() != null) {
             return student;
         }
-        throw new NotFoundException("Студент не имеет назначеного факультета.");
+        throw new NotFoundException("Студент не имеет назначенного факультета.");
     }
 }
