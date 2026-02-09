397cf4a2798933d8751633cb49fb350ecf1ef1cf
Bodnevich <bodnevichkristina@mail.ru>
2026-02-09 23:00:13 +0300

    Hogwards SchoolHW6

--- src/main/java/ru/hogwarts/school/config/SwaggerConfig.java
+++ src/main/java/ru/hogwarts/school/config/SwaggerConfig.java
@@ -3,14 +3,9 @@ package ru.hogwarts.school.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.parameters.RequestBody;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
 
 @Configuration
 public class SwaggerConfig {
